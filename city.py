4dda4634ce94aaa8a80da8716e49db960cebaa0e
zibin <chenzibin2017@outlook.com>
2023-10-15 14:47:15 -0400

    Initial commit

---
+++ city.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+from graph import Graph, Vertex
+
+class City:
+    def __init__(self):
+        """
+        Constructor of the City class
+        """
+        self.city_map = None
+
+    def buildMap(self, filename):
+        """
+
+        :param filename:
+        :return:
+        """
+        pass
+
