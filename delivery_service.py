196b31d5765914ef247fe8ed4c6ec8bc4dede318
zibin <chenzibin2017@outlook.com>
2023-10-19 16:41:57 -0400

    add test cases

--- delivery_service.py
+++ delivery_service.py
@@ -2,14 +2,14 @@
 UMass ECE 241 - Advanced Programming
 Project 2 - Fall 2023
 """
-
+import sys
 from graph import Graph, Vertex
 from priority_queue import PriorityQueue
 
 class DeliveryService:
     def __init__(self) -> None:
         """
-        Constructor of the City class
+        Constructor of the Delivery Service class
         """
         self.city_map = None
         self.MST = None
@@ -67,6 +67,39 @@ class DeliveryService:
         :return:
         """
         pass
+
+
+    ## DO NOT MODIFY CODE BELOW!
+    @staticmethod
+    def nodeEdgeWeight(v):
+        return sum([w for w in v.connectedTo.values()])
+
+    @staticmethod
+    def totalEdgeWeight(g):
+        return sum([DeliveryService.nodeEdgeWeight(v) for v in g]) // 2
+
+    @staticmethod
+    def checkMST(g):
+        for v in g:
+            v.color = 'white'
+
+        for v in g:
+            if v.color == 'white' and not DeliveryService.DFS(g, v):
+                return 'Your MST contains circles'
+        return 'MST'
+
+    @staticmethod
+    def DFS(g, v):
+        v.color = 'gray'
+        for nextVertex in v.getConnections():
+            if nextVertex.color == 'white':
+                if not DeliveryService.DFS(g, nextVertex):
+                    return False
+            elif nextVertex.color == 'black':
+                return False
+        v.color = 'black'
+
+        return True
 
 # NO MORE TESTING CODE BELOW!
 # TO TEST YOUR CODE, MODIFY test_delivery_service.py
